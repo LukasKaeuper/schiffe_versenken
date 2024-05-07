74bc58411c3b46258c7b6d37c47029c4327a6f0f
LukasKaeuper <lukas.kaeuper@web.de>
2024-05-07 11:48:51 +0200

    Klassen erstellt und Spielfeld bearbeitet

--- src/Spielfeld.java
+++ src/Spielfeld.java
@@ -1,2 +1,28 @@
 public class Spielfeld {
+private int groesse = 10;
+private char[][] spielfeld = new char[groesse][groesse];
+
+    public void erstelleSpielfeld(){
+        for (int i = 0; i < groesse; i++){
+            for (int j = 0; j < groesse; j++){
+                spielfeld[i][j] = '-';
+            }
+        }
+    }
+
+
+
+
+    public void setGroesse(int x){
+        this.groesse = x;
+    }
+
+    public int getGroesse(){
+        return groesse;
+    }
+
+    public void anzeigen(){
+        System.out.println(groesse);
+    }
+
 }
